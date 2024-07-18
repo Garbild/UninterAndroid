ba0a57d4663985df16168d5ec4527c3d19cefcac
Bowie <marketing@letscode.com.br>
2024-07-18 20:20:52 -0300

    criando os layouts e estilizando

--- DemosAndroid/app/src/main/java/com/uninter/demosandroid/filmesuninter/domain/entity/Filme.kt
+++ DemosAndroid/app/src/main/java/com/uninter/demosandroid/filmesuninter/domain/entity/Filme.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
-@Entity(tableName = "filme")
+@Entity(tableName = "filme")//entidade
 data class Filme(
     @PrimaryKey val id: Int,
     val name: String,
